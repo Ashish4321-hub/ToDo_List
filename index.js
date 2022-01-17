6446441de0c380cc45e009611d5aceb6cbed01c0
Ashish4321-hub <anmol2001anu@gmail.com>
2022-01-17 14:38:40 +0530

    major changes on js

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
-document.querySelector(".btn").addEventListener("click",function(){document.querySelector(".para").append(document.querySelector(".inpt").value+"\n");})
+//document.querySelector(".btn").addEventListener("click",function(){document.querySelector(".p").append(document.querySelector(".inpt").value+"\n");})
+//document.querySelector(".p").addEventListener("click",function(){document.querySelector(".p").remove(document.querySelector(".p").innerText);})
+
+document.querySelector(".btn").addEventListener("click",function(){node = document.getElementById('c');
+node.insertAdjacentHTML('afterend', '<div class="n"><h3></h3></div>');
+document.querySelector(".btn").addEventListener("click",function(){document.querySelector(".n").innerHTML=("<textarea>" + document.querySelector(".inpt").value+ "</textarea>   <button >Delete</button>\n");})
+})
+document.querySelector(".n").addEventListener("click",function(){
+  //document.querySelector(".n").innerText="bye"
+alert("clicked");})
+
