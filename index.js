e9cbcfba36a6991e7369b0abab9c69712cce1d47
Ashish4321-hub <anmol2001anu@gmail.com>
2022-01-17 16:01:46 +0530

    minor changes on js

--- index.js
+++ index.js
@@ -4,9 +4,10 @@
 
 document.querySelector(".btn").addEventListener("click",function(){node = document.getElementById('c');
 node.insertAdjacentHTML('afterend', '<div class="n"><h3></h3></div>');
-document.querySelector(".btn").addEventListener("click",function(){document.querySelector(".n").innerHTML=("<textarea>" + document.querySelector(".inpt").value+ "</textarea>   <button >Delete</button>\n");})
-})
-document.querySelector(".n").addEventListener("click",function(){
-  //document.querySelector(".n").innerText="bye"
-alert("clicked");})
+document.querySelector(".btn").addEventListener("click",function(){document.querySelector(".n").innerHTML=("<textarea>" + document.querySelector(".inpt").value+ "</textarea>   <button class='del'>Delete</button>\n");})
 
+document.querySelector(".del").addEventListener("click",function(){
+  //document.querySelector(".n").innerText="bye"
+  document.querySelector(".n").remove();
+})
+})
